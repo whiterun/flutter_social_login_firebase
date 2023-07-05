745209737698aefbf92a27dc51af9cb4fb843808
Rengga Aditya <mbp@192.168.1.4>
2023-07-06 00:36:09 +0700

    integrate with laravel passport

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 import '../views/user_info_view.dart';
 
@@ -75,6 +77,9 @@ class Authentication {
               await auth.signInWithCredential(credential);
 
           user = userCredential.user;
+
+          final token = await userCredential.user?.getIdTokenResult();
+          await exchangeWithPassport(token?.token);
         } on FirebaseAuthException catch (e) {
           if (e.code == 'account-exists-with-different-credential') {
             if (context.mounted) {
@@ -124,6 +129,21 @@ class Authentication {
           content: 'Error signing out. Try again.',
         ),
       );
+    }
+  }
+
+  static Future<void> exchangeWithPassport(String? token) async {
+    const url = 'http://localhost:8000/api/login';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'provider': 'google', 'token': token}),
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint(response.body.toString());
+    } else {
+      debugPrint('Token Exchange Failed');
     }
   }
 }
